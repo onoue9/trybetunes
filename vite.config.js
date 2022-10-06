a65c617a5433dbea6c838b3d02e7977834183ade
Filipe Onoue <filipeflom@gmail.com>
2022-10-06 13:52:08 -0300

    feat: :sparkles: include plugin Pages() to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [Pages(), react()],
   base: 'trybetunes'
 })
