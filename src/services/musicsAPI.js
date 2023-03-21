55c70c875b746ac3395f602d0aa925775c767130
Filipe Onoue <filipeflom@gmail.com>
2023-03-21 17:15:06 -0300

    feat: :sparkles: include a header in fetch request

--- src/services/musicsAPI.js
+++ src/services/musicsAPI.js
@@ -2,7 +2,11 @@ const getMusics = async (id) => {
   let request = null
 
     try {
-      request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`);
+      request = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=song`, {
+        headers: {
+          'Origin': 'https://onoue9.github.io'
+        }
+      });
     } catch (error) {
       console.log(error);
     }
